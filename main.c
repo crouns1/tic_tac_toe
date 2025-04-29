d1c500e18261131de84273849b6bfdf6e019274a
crouns1 <jamalaitchdid1@gmail.com>
2025-04-29 02:04:25 +0100

    now iam in infinte loop i need to create an enemy to play against user and find a good algo i will work with minmax algo to create this unbeatable enemy AI

--- main.c
+++ main.c
@@ -36,8 +36,36 @@ void printBoard() {
     printf("\n\n");
 }
 
+
+void humanMove() {
+    int row, col;
+    while (1) {
+        printf("Enter row and column (1-3): ");
+        scanf("%d %d", &row, &col);
+        row--; 
+	col--;
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == EMPTY) {
+            board[row][col] = HUMAN;
+            break;
+        } else {
+            printf("Invalid move. Try again.\n");
+        }
+    }
+}
+
 int main() {
     initBoard();
     printBoard();
+    char winner = 'N';
+
+    while (winner == 'N') {
+        humanMove();
+        printBoard();
+    }
+
+    if (winner == HUMAN)
+        printf("You win!\n");
+    
+    return 0;
 }
 
